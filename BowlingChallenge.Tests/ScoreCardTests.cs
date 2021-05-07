bdfd05f1f5edd364ed550ca2da8d620dbc76ae3a
Dwayne McMurrian <dwayne@mcmurrian.com>
2021-05-07 00:49:57 -0700

    began outlining testcases

---
+++ BowlingChallenge.Tests/ScoreCardTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BowlingChallengeTests
+{
+   public class ScoreCardTests
+    {
+        //testcases
+            //add frame
+                //basic add    
+                    //add a new frame to frames
+                    //current Frame set
+                    //totalupdated...only if 0
+                //add spare with previous strike
+                    //set previous to 20. update total
+                //add strike with previous spare
+                    //set previous to 20. update total
+                //add value after spare
+                    //10 plus first ball
+                //add value after strike
+                    //10 plus next two values
+         //get current total
+            //returns current scorecard total
+
+        //  • List<frames>
+        //	• Int current frame;
+        //	• current Score
+
+    }
+}
